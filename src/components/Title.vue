593dcac960c46c779653d02b249fe1126138e4c6
cincenfrank <fpetrangeli1@gmail.com>
2021-11-10 15:20:51 +0100

    created title component

---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>La mia prima App con VUE CLI!</h1>
+</template>
+
+<script>
+export default {
+name: "Title"
+}
+</script>
+
+<style>
+h1{
+    color: red;
+    font-weight: bold;
+}
+</style>
